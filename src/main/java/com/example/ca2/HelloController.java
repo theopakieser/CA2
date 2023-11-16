da85ca735d3f9a1169d9f8b6b7661b7708cb02ab
theopakieser <20101918@mail.wit.ie>
2023-11-16 17:39:05 +0000

    buttons <3

--- src/main/java/com/example/ca2/HelloController.java
+++ src/main/java/com/example/ca2/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.ca2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+     @FXML
+     private Label exitText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
+    @FXML
+    protected void onExitClicked() {
+        Platform.exit();
+    }
+
+    @FXML
+    protected void onGamesClicked() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        Stage stage = new Stage();
+        stage.setTitle("The Game Menu!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
